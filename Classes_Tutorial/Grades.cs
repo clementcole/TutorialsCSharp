06b32f82ad9aaa79098ae653a69387b6b08a18af
ccole <ccole@IT-LT06>
2018-05-11 16:15:19 -0500

    Add project files.

---
+++ Classes_Tutorial/Grades.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grades
+{ 
+    class GradeBook
+    {
+
+        private List<double> grades; //Stop the grades uninitialized exception
+        public static double Max = 100;
+        public static double Min = 0;
+        private double average;
+        private double highest;
+        private double lowest;
+        private double median;
+        
+        public GradeBook()
+        {
+            grades = new List<double>();
+        }
+        public void AddGrade(double grade )
+        {
+            grades.Add(grade);
+        }
+        public GradeStatistics ComputeStatistics()
+        {
+            //Will be populated to generate the statistics for grade
+            GradeStatistics stats =  new GradeStatistics();
+            float sum = 0;
+            
+            foreach (float grade in grades)
+            {
+                sum += grade;
+            }
+
+            return stats;
+        }
+        
+       
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Classes_Tutorial/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * 
+ * We need an electronic grade book to read the scores of an individual student and then compute some simple statistics from the scores.
+ * The grades are entered as floating point numbers from 0 to 100, and the statistics should show us the highest grade, the lowest grade, and 
+ * the average grade. 
+ * 
+ */
+namespace Grades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GradeBook book = new GradeBook();
+            book.AddGrade(91);
+            book.AddGrade(60);
+            book.AddGrade(60.3);
+            book.AddGrade(83.4);
+            book.AddGrade(43.2);
+            book.AddGrade(93.6);
+            book.AddGrade(84.5);
+            book.AddGrade(92.3);
+
+
+            GradeStatistics stats = book.ComputeStatistics();
+            book.ComputeStatistics();
+        }
+    }
+}
